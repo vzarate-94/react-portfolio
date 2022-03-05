7646d749b8d416027efc1f41b7d84f31fd7f2231
vzarate26 <vzarate26@gmail.com>
2022-03-04 21:37:12 -0500

    build Nav component and export to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './components/Home/Home'
+import Nav from './components/Nav/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Home />
     </div>
   );
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css'
+
+function Nav() {
+  return (
+    <nav>
+      <h1>Home</h1>
+      <h2>IDK</h2>
+    </nav>
+  )
+}
+
+export default Nav
