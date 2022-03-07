ce7a3a9a5ae0e82829eca0159b09cf3c9ba638ba
vzarate26 <vzarate26@gmail.com>
2022-03-07 17:55:13 -0500

    create project data file with array of objects for project component

---
+++ src/assets/Data/projectData.js
@@ -0,0 +1,37 @@
+import GoldenBoys from '../images/GoldenBoys.png'
+import Original from '../images/Original.png'
+import quizGame from '../images/quizGame.png'
+import scourseCard from '../images/scourseCard.png'
+import WeTravel from '../images/WeTravel.png'
+import sanMiguel from '../images/sanMiguel.png'
+
+
+
+const projectData = [
+  {
+    img: GoldenBoys,
+    title: "Golden Boys"
+  },
+  {
+    img: Original,
+    title: "Golden Boys"
+  },
+  {
+    img: quizGame,
+    title: "Golden Boys"
+  },
+  {
+    img: scourseCard,
+    title: "Golden Boys"
+  },
+  {
+    img: WeTravel,
+    title: "Golden Boys"
+  },
+  {
+    img: sanMiguel,
+    title: "Golden Boys"
+  },
+]
+
+export default projectData
