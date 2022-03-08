b5aef1c1593ffaf641d134c0aef91595bbf0246e
vzarate26 <vzarate26@gmail.com>
2022-03-08 18:49:32 -0500

    complete contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Nav from '../Nav/Nav'
+import myLogoWhite from '../../assets/images/myLogoWhite.png'
+import '../../App.css'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <>
+    <Nav />
+    <footer className='footer'>
+        <img src={myLogoWhite} alt="victor logo" className='logo' />
+        <div id="contact-me"><h2>CONTACT ME</h2></div>
+        <div className='contact-info'>
+          <div className='contact-link'><a 
+            href='mailto:vzarate26@gmail.com'>vzarate26@gmail.com<i className="fas fa-paper-plane"></i> | </a></div>
+          <div className='contact-link'><a href="tel:4348256618"> (434) 825-6618 <i class="fas fa-phone-square"></i> |   </a></div>
+          <div className='contact-link'><a href="https://www.linkedin.com/in/victor-zarate-bustamante/" target="blank"> LinkedIn <i class="fab fa-linkedin"></i> | </a></div>
+          <div className='contact-link'><a href="https://github.com/vzarate-94" target="blank"> Github <i class="fab fa-github"></i> | </a></div>
+          <div className='contact-link'><a href="https://www.instagram.com/vicz113/" target="blank">Instagram <i class="fab fa-instagram"></i></a></div>
+        </div>
+      </footer>
+    </>
   )
 }
 
