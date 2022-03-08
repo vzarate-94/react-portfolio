2f32641028675da1c553059787715ea33e7fad53
vzarate26 <vzarate26@gmail.com>
2022-03-07 23:19:05 -0500

    add links to nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,12 +5,15 @@ import '../../App.css'
 function Nav() {
   return (
     <nav>
-      
       <Link to="/">
       <button className="btn" type="button"><strong>Home</strong></button>
       </Link>
-      <h3>Portfolio</h3>
-      <h3>Contact</h3>
+      <Link to="/portfolio">
+      <button className="btn" type="button"><strong>Portfolio</strong></button>
+      </Link>
+      <Link to="/contact">
+      <button className="btn" type="button"><strong>Contact</strong></button>
+      </Link>
     </nav>
   )
 }
