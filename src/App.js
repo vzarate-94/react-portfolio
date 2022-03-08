4880322a1db4ffd5dd7d48152b7630d278d1e870
vzarate26 <vzarate26@gmail.com>
2022-03-08 18:10:51 -0500

    create contact component page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home/Home'
 import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
 
 
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/portfolio' element={<Portfolio />} />
+      <Route path='/contact' element={<Contact />} />
     </Routes>
 
     </div>
