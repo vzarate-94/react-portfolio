6d43634387c980cf24efadb3bd23453712cda76f
vzarate26 <vzarate26@gmail.com>
2022-03-14 15:50:14 -0400

    create Technologies component and add to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home/Home'
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
+import Technologies from './components/Technologies/Technologies';
 
 
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/portfolio' element={<Portfolio />} />
+      <Route path='/technologies' element={<Technologies />} />
       <Route path='/contact' element={<Contact />} />
     </Routes>
 
---
+++ src/components/Technologies/Technologies.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+
+function Technologies() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  )
+}
+
+export default Technologies
